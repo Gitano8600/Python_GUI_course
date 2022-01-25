7ad6e0b5f1ea054edb4ed00249716d0e8d88360a
Manuel <manuel.nellen@glattnet.ch>
2022-01-25 22:17:07 +0100

    quick summary in print mode

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 from config import cmc_api_key
+from portfolio import portfolio
 
 url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 parameters = {
   'start':'1',
-  'limit':'5',
-  'convert':'USD'
+  'limit':'1000',
+  'convert':'CHF'
 }
 headers = {
   'Accepts': 'application/json',
@@ -20,9 +21,19 @@ session.headers.update(headers)
 try:
     response = session.get(url, params=parameters)
     data = json.loads(response.text)
-    print(data)
-    print(data['data'][0]['symbol'])
-    print(data['data'][0]['quote']['USD']['price'])
+    # print(data)
 except (ConnectionError, Timeout, TooManyRedirects) as e:
     print(e)
 
+total_value = 0
+for i in data['data']:
+    for coin in portfolio:
+        if i['symbol'] == coin['symbol']:
+            print(i['symbol'])
+            print('{0:.2f}'.format(i['quote']['CHF']['price']))
+            cur_pos_value = round(coin["amount_owned"] * float(i["quote"]["CHF"]["price"]), 2)
+            total_value += cur_pos_value
+            print(f'TOTAL CHF VALUE = {cur_pos_value}')
+            print('-----------------------')
+
+print(f'FINAL VALUE = {total_value}')
